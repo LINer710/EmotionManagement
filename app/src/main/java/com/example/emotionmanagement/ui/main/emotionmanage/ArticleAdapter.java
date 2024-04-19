c15e8b51bf9542a6d805c06345516ab288e9dd97
曹小林 <caoxiaolin@yupaowang.com>
2024-04-19 22:48:14 +0800

    情绪管理文章观看次数处理

--- app/src/main/java/com/example/emotionmanagement/ui/main/emotionmanage/ArticleAdapter.java
+++ app/src/main/java/com/example/emotionmanagement/ui/main/emotionmanage/ArticleAdapter.java
@@ -1,8 +1,9 @@
 package com.example.emotionmanagement.ui.main.emotionmanage;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,7 +16,20 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.example.emotionmanagement.R;
 
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHolder> {
 
@@ -54,7 +68,7 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, @SuppressLint("RecyclerView") int position) {
         Article article = articles.get(position);
         holder.textTitle.setText(article.getTitle());
         holder.textContent.setText(article.getShortenedContent()); // 使用截取后的内容
@@ -64,9 +78,9 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
                 Intent intent = new Intent(context, ArticleDetailActivity.class);
                 intent.putExtra("url", article.getUrl());
                 context.startActivity(intent);
+                incrementViews(article.getResourceId(), position, holder);
             }
         });
-
 
         holder.textViews.setText(String.valueOf(article.getViews()));
         holder.imageFavorite.setImageResource(article.isFavorite() ? R.drawable.ic_favorite_outline : R.drawable.ic_no_favorite_outline);
@@ -81,6 +95,48 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
                     .load(imageUrl)
                     .into(holder.imageArticle);
         }
+    }
+
+    private void incrementViews(int resourceId, int position, ViewHolder holder) {
+        OkHttpClient client = new OkHttpClient();
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("resource_id", resourceId);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        RequestBody requestBody = RequestBody.create(jsonBody.toString(), MediaType.parse("application/json; charset=utf-8"));
+        Request request = new Request.Builder()
+                .url("http://192.168.68.170:5000/increment_views")
+                .post(requestBody)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String responseData = response.body().string();
+                    int updatedCount = 0;
+                    updatedCount = articles.get(position).getViews() + 1;
+
+                    final int finalUpdatedCount = updatedCount;
+                    if (context instanceof Activity) {
+                        ((Activity) context).runOnUiThread(() -> {
+                            Article article = articles.get(position);
+                            article.setViews(finalUpdatedCount); // 更新模型数据
+                            holder.textViews.setText(String.valueOf(finalUpdatedCount)); // 更新视图
+                            notifyItemChanged(position); // 仅刷新修改的项
+                        });
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     @Override
